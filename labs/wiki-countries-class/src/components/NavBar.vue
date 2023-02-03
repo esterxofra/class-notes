466b3371d9756583c7f0a64e4ab24a1f966e03d6
Ester Xofra <esterxofra@gmail.com>
2023-02-03 19:44:49 +0100

    lab1

---
+++ labs/wiki-countries-class/src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<!-- HTML HERE -->
+<template>
+  <nav class="navbar navbar-expand-lg bg-primary">
+    <RouterLink class="text-white" to="/">Wiki Countries</RouterLink>
+  </nav>
+</template>
+<!-- JS HERE -->
+<script setup>
+import { ref, reactive } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+</script>
